c152717d254ad2888c908a0be942524bc431f4b9
rsdiaz <robertoserrano83@gmail.com>
2023-03-06 01:59:37 +0100

    custom theme for sidebar

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,3 +1,10 @@
 import { CustomFlowbiteTheme } from "flowbite-react";
 
-export const theme: CustomFlowbiteTheme = {}
+export const theme: CustomFlowbiteTheme = {
+  sidebar: {
+    root: {
+      inner:
+        'h-full overflow-y-auto overflow-x-hidden rounded bg-white py-4 px-3 dark:bg-gray-800 border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700',
+    },
+  },
+}
