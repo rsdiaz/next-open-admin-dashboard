dc21c4b4aa4b6af10f0ee5e9eeab27e2e143c979
rsdiaz <robertoserrano83@gmail.com>
2023-03-07 01:07:19 +0100

    set dark theme value in local storage

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,9 @@
-import appConfig from "@/config/appConfig"
-import { DarkThemeToggle } from "flowbite-react"
-import Image from "next/image"
-import { useSidebarContext } from "../context/SidebarContext"
+import appConfig from '@/config/appConfig'
+import { DarkThemeToggle } from 'flowbite-react'
+import Image from 'next/image'
+import { useSidebarContext } from '../context/SidebarContext'
+import { useLocalStorage } from '@/hooks/useLocalStorage'
+import { useDarkMode } from '@/hooks/useDarkMode'
 
 const MenuToggle = () => {
   const { isOpenOnSmallScreens, isPageWithSidebar, setOpenOnSmallScreens } =
@@ -15,9 +17,33 @@ const MenuToggle = () => {
       onClick={() => setOpenOnSmallScreens(!isOpenOnSmallScreens)}
     >
       {!isOpenOnSmallScreens ? (
-        <svg id="toggleSidebarMobileHamburger" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+        <svg
+          id="toggleSidebarMobileHamburger"
+          className="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
       ) : (
-        <svg id="toggleSidebarMobileClose" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+        <svg
+          id="toggleSidebarMobileClose"
+          className="w-6 h-6"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
       )}
     </button>
   )
@@ -26,8 +52,16 @@ const MenuToggle = () => {
 const NavbarLogo = () => {
   return (
     <a href="" className="flex ml-2 md:mr-24">
-      <Image width={28} height={28} src={appConfig.logoUrl} className="h-8 mr-3" alt="FlowBite Logo" />
-      <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">{appConfig.appName}</span>
+      <Image
+        width={28}
+        height={28}
+        src={appConfig.logoUrl}
+        className="h-8 mr-3"
+        alt="FlowBite Logo"
+      />
+      <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+        {appConfig.appName}
+      </span>
     </a>
   )
 }
@@ -35,12 +69,31 @@ const NavbarLogo = () => {
 const NavbarSearch = () => {
   return (
     <form action="#" method="GET" className="hidden lg:block lg:pl-3.5">
-      <label htmlFor="topbar-search" className="sr-only">Search</label>
+      <label htmlFor="topbar-search" className="sr-only">
+        Search
+      </label>
       <div className="relative mt-1 lg:w-96">
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+          <svg
+            className="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </div>
-        <input type="text" name="email" id="topbar-search" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search" />
+        <input
+          type="text"
+          name="email"
+          id="topbar-search"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+          placeholder="Search"
+        />
       </div>
     </form>
   )
@@ -48,23 +101,51 @@ const NavbarSearch = () => {
 
 const MobileSearch = () => {
   return (
-    <button id="toggleSidebarMobileSearch" type="button" className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+    <button
+      id="toggleSidebarMobileSearch"
+      type="button"
+      className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+    >
       <span className="sr-only">Search</span>
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
     </button>
   )
 }
 
 const NavbarNotification = () => {
   return (
-    <button type="button" data-dropdown-toggle="notification-dropdown" className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700">
+    <button
+      type="button"
+      data-dropdown-toggle="notification-dropdown"
+      className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700"
+    >
       <span className="sr-only">View notifications</span>
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path></svg>
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z"></path>
+      </svg>
     </button>
   )
 }
 
 const Navbar = () => {
+  const { darkMode, setDarkMode } = useDarkMode()
+
   return (
     <nav className="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
@@ -77,7 +158,10 @@ const Navbar = () => {
           <div className="flex items-center">
             <MobileSearch />
             <NavbarNotification />
-            <DarkThemeToggle theme={{}} />
+            <DarkThemeToggle
+              theme={{}}
+              onClick={() => setDarkMode(!darkMode)}
+            />
           </div>
         </div>
       </div>
