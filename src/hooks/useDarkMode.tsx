45649e1f905769f333df24876189b80cded07329
rsdiaz <robertoserrano83@gmail.com>
2023-03-09 00:49:19 +0100

    refactor dark mode hook

--- src/hooks/useDarkMode.tsx
+++ src/hooks/useDarkMode.tsx
@@ -1,24 +1,5 @@
-import { useEffect } from 'react'
-import { useLocalStorage } from './useLocalStorage'
+import { SettingsContext, SettingsContextValue } from '@/context/SettingContext'
+import { useContext } from 'react'
 
-// Hook
-export function useDarkMode() {
-  const [darkMode, setDarkMode] = useLocalStorage<boolean>(
-    'dark-mode-enabled',
-    false
-  )
-
-  const enabled = typeof darkMode !== 'undefined' && darkMode
-
-  useEffect(() => {
-    const className = 'dark'
-    const element = window.document.body
-    if (enabled) {
-      element.classList.add(className)
-    } else {
-      element.classList.remove(className)
-    }
-  }, [enabled])
-
-  return { darkMode, setDarkMode }
-}
+export const useSettings = (): SettingsContextValue =>
+  useContext(SettingsContext)
