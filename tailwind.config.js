515bda60ab0052cc605bfc0f5c666b4aca55ecc6
rsdiaz <robertoserrano83@gmail.com>
2023-03-09 01:14:28 +0100

    prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{ts,tsx}",
-    "./public/**/*.html",
-    "./src/**/*.{ts,tsx}",
+    './node_modules/flowbite-react/**/*.js',
+    './pages/**/*.{ts,tsx}',
+    './public/**/*.html',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
+  plugins: [require('flowbite/plugin')],
 }
