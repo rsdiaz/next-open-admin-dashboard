b24c766573705e3390e220cbeb5cb70ca8696e6d
rsdiaz <robertoserrano83@gmail.com>
2023-03-12 00:36:21 +0100

    custom modal theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -7,4 +7,9 @@ export const theme: CustomFlowbiteTheme = {
         'h-full overflow-y-auto overflow-x-hidden rounded bg-white py-4 px-3 dark:bg-gray-800 border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700',
     },
   },
+  modal: {
+    content: {
+      inner: 'relative bg-white rounded-lg shadow dark:bg-gray-800',
+    },
+  },
 }
