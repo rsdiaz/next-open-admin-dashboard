729572eef8dd961ae030509952f8df0dbb94e9cd
Roberto Serrano <robertoserrano83@gmail.com>
2023-06-12 00:52:10 +0200

    Update [...nextauth].ts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
-const YOUR_API_URL = 'https://api.multipublicador.grupolivamotor.com/signin'
+const YOUR_API_URL = ''
 
 export const authOptions = {
   providers: [
